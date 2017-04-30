15c1420660bc8f321e370c1d6b57821e4e4b0bd2
Jamie Holding <me@jamie.holdings>
2017-04-30 15:09:53 +0100

    Add polite message to developers to suggest helping us

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ const Home = () => <div>
   <p><i>What?</i></p>
   <p>AlphaZone4 may return in full-form at some point, but until I have the time to get it all back up and running, I'm restoring the PlayStation Home database in archive mode.</p>
   <p>You can see how this site is developing on our GitHub page at <a href='https://github.com/alphazone4'>https://github.com/alphazone4</a>. The full source of the site frontend will be available, and also the majority of the source code that made up the original AlphaZone4 website when PlayStation Home was still online (in case you were curious how we did it...)</p>
+
+  <h2>Virtual World Developers</h2>
+  <p>Run a virtual world? Please get in contact and open up your APIs! We'd love to catalogue your world's items, providing you with an easy to navigate web-based view of your world.</p>
 </div>
 
 export default Home;
